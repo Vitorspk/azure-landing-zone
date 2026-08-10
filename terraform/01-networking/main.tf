014b256a917f7966f94aaf030900808ec0d6447a
Vitorspk <vitor.schiavo@verifymyage.co.uk>
2026-08-09 21:31:42 -0300

    fix: remove unrestricted SSH ingress rule (no legitimate use case in this AKS architecture)

--- terraform/01-networking/main.tf
+++ terraform/01-networking/main.tf
@@ -43,18 +43,6 @@ resource "azurerm_network_security_group" "allow_ssh" {
   tags                = var.tags
 
   security_rule {
-    name                       = "AllowSSH"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
     name                       = "AllowHTTPS"
     priority                   = 110
     direction                  = "Inbound"
